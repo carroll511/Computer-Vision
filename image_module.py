030c484cde22fc791353bab71a5d3cdc125f44a0
carroll511 <141486883+carroll511@users.noreply.github.com>
2024-11-11 11:29:13 +0900

    Delete image_module.py

--- image_module.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-def load_lenna():
-    lenna = cv2.imread('lenna.png', cv2.IMREAD_GRAYSCALE)
-    return lenna
-
-def load_shapes():
-    shapes = cv2.imread('shapes.png', cv2.IMREAD_GRAYSCALE)
-    return shapes
